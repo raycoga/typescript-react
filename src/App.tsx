20ba5ddaf5c384ea466924125d1af18d1d9faf10
raycoga <capram94@gmail.com>
2020-12-24 15:44:57 -0400

    Primer comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
 
-function App() {
+type ReactForm = React.FormEvent<HTMLFormElement>;
+type inputForm = HTMLInputElement;
+
+interface ObjTask {
+  name: string|undefined;
+  done: boolean;
+}
+
+const initialStateTask: string|undefined=undefined // union property
+
+function App(): JSX.Element {
+  const [newTask, setnewTask] = useState(initialStateTask);
+  const [taskList, settaskList] = useState<ObjTask[]>([]);
+  const inputForm = useRef<inputForm>(null);
+
+  const handleSubmit = (e: ReactForm): void => {
+    e.preventDefault();
+    addNewTask(newTask);
+    setnewTask("");
+    inputForm.current?.focus()
+  };
+
+  const addNewTask = (name: string|undefined): void => {
+    let tasks: ObjTask[] = [...taskList];
+    tasks.unshift({
+      name,
+      done: false,
+    });
+    settaskList(tasks);
+  };
+
+  const markTask = (i: number): void => {
+    let tasks: ObjTask[] = [...taskList];
+    tasks[i].done = !tasks[i].done;
+    settaskList(tasks);
+  };
+  const removeTask = (i: number): void => {
+    let tasks: ObjTask[] = [...taskList];
+    tasks.splice(i, 1);
+    settaskList(tasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h1 style={{textAlign:'center'}}>TO-DO CON TYPESCRIPT</h1>
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input
+                  className="form-control"
+                  type="text"
+                  value={newTask}
+                  onChange={(e) => setnewTask(e.target.value)}
+                  autoFocus
+                  ref={inputForm}
+                />
+                <button className="btn btn-success btn-block mt-2">
+                  accept
+                </button>
+              </form>
+            </div>
+          </div>
+          {taskList.map((e: ObjTask, key: number) => (
+            <div className="card card-body mt-2" key={key}>
+              <h2 style={{ textDecoration: e.done ? "line-through" : "" }}>
+                {e.name}
+              </h2>
+              <div>
+                <button
+                  className={`btn ${e.done?'btn-success':'btn-secondary'}`}
+                  onClick={(_) => markTask(key)}
+                >
+                  {e.done ? "✓" : "✗"}
+                </button>
+
+                <button className="btn" onClick={(_) => removeTask(key)}>
+                  🗑
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'bootswatch/dist/sketchy/bootstrap.min.css'
 ReactDOM.render(
   <React.StrictMode>
     <App />
